cdcb8bb2dc03a691db60dc6cc2f17f0d9ce4a5ae
Ky Waegel <kwaegel@users.noreply.github.com>
2016-12-03 20:00:09 -0800

    Day 3, part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -13,9 +13,10 @@ fn main() {
     let mut file = File::open("input.txt").unwrap();
     let _ = file.read_to_string(&mut input_string);
 
+    // Part 1: process by rows
     let mut valid = 0;
     for line in input_string.lines() {
-        let values = line.split(" ")
+        let values = line.split(' ')
             .filter(|ref val| val.len() > 0) // Filter contiguous separators.
             .map(|ref val| val.parse().unwrap())
             .collect::<Vec<u32>>();
@@ -25,4 +26,25 @@ fn main() {
     }
     println!("Day 1: valid triangles = {}", valid);
     assert!(valid == 1050);
+
+    // Part 2: process by column in groups of three.
+    let all_values = input_string.split(|c| c == ' ' || c == '\n')
+        .filter(|ref val| val.len() > 0) // Filter contiguous separators
+        .map(|ref val| val.parse().unwrap()) // Convert to integer
+        .collect::<Vec<u32>>();
+
+    // Process values with a stride of 9 (for each block of three triangles)
+    let mut valid_by_column = 0;
+    for block_stride in 0..all_values.len()/9 {
+        for stride in 0..3 {
+            let a = all_values[block_stride*9 + stride + 0];
+            let b = all_values[block_stride*9 + stride + 3];
+            let c = all_values[block_stride*9 + stride + 6];
+            if valid_triangle(a,b,c) {
+                valid_by_column += 1;
+            }
+        }
+    }
+    println!("Day 1: valid triangles by column = {}", valid_by_column);
+
 }
