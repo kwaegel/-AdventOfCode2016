d54618bf27c359ad070ddc9d094c4a775d7d7c83
Ky Waegel <kwaegel@users.noreply.github.com>
2016-12-05 00:42:13 -0800

    Optimize day 5, part 1 for speed

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -6,15 +6,17 @@ extern crate crypto;
 use crypto::md5::Md5;
 use crypto::digest::Digest;
 
-// Returns the sixth character if the first five are zero
-fn sixth_char_after_zeros(hex_str: &str) -> Option<char> {
+// It takes two hex digits to make a byte, so check
+// the first 2.5 bytes for zero values
+fn first_five_hex_zero(input: &[u8; 16]) -> bool {
+    let sum = (input[0] as u32) + (input[1] as u32) + (input[2] as u32 >> 4);
+    sum == 0
+}
 
-    for i in 0..5 {
-        if hex_str.as_bytes()[i] != '0' as u8 {
-            return None;
-        }
-    }
-    Some(hex_str.as_bytes()[5] as char)
+fn get_sixth_hex_char(input: &[u8; 16]) -> char {
+    let mut buff = String::new();
+    let _ = write!(buff,"{:x}", input[2]);
+    buff.as_bytes()[0] as char
 }
 
 fn main() {
@@ -26,6 +28,9 @@ fn main() {
     let mut password = "".to_owned();
     let target_password_length = 8;
 
+    // Static buffer to hold the result
+    let mut hash_output = [0u8; 16];
+
     // Loop through door_id + [0..max] to find hashes that
     // start with five zeros
     for i in 0..u32::max_value() {
@@ -33,14 +38,15 @@ fn main() {
         let _ = write!(test_value,"{}{}", door_id, i);
         hasher.reset();
         hasher.input(test_value.as_bytes());
-        
-        let hash_result = hasher.result_str();
-        if let Some(next_char) = sixth_char_after_zeros(&hash_result) {
-            println!("{}", hash_result);
+        hasher.result(&mut hash_output);
+
+        if first_five_hex_zero(&hash_output) {
+            let next_char = get_sixth_hex_char(&hash_output);
+            //let next_char = hash_output[0] as char;
             password.push(next_char);
         }
 
-        if password.len() == target_password_length {
+        if password.len() >= target_password_length {
             break;
         }
     }
@@ -59,6 +65,12 @@ fn test1() {
     hasher.input(test_value.as_bytes());
     let hash_result = hasher.result_str();
     println!("{}", hash_result);
+
+    let mut buff = [0;16];
+    hasher.result(&mut buff);
+    let test_char1 = get_sixth_hex_char(&buff);
+    println!("{}", test_char1);
+    //assert!(test_char1 == '');
 
     let test_value2 = "abc5017308";
     hasher.reset();
