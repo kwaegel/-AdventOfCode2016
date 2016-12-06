9c6ea6693da49a8f3deda8c577a632a5f1ae1e8a
Ky Waegel <kwaegel@users.noreply.github.com>
2016-12-05 23:58:48 -0800

    Day 6, part 2.

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -5,28 +5,51 @@ use std::io::Read;
 fn main() {
     let mut input_string = String::new();
     let mut file = File::open("input.txt").unwrap();
+    // let mut file = File::open("test_data.txt").unwrap();
     let _ = file.read_to_string(&mut input_string);
 
-    const LENGTH: usize = 8;
-    let mut histograms = [[0i32; 128]; LENGTH];
+    const MAX_LENGTH: usize = 8;
+    let mut histograms = [[0i32; 128]; MAX_LENGTH];
 
     for word in input_string.lines() {
         let bytes = word.as_bytes();
-        for i in 0..LENGTH {
+        for i in 0..bytes.len() {
             histograms[i][bytes[i] as usize] += 1;
         }
     }
 
     // Print most common value for each column
-    let mut word = String::new();
-    for i in 0..LENGTH {
-        if let Some(val_pair) = histograms[i].iter().enumerate().map(|(x, y)| (y, x)).max() {
+    let mut most_common = String::new();
+    let mut least_common = String::new();
+    for i in 0..MAX_LENGTH {
+        // Most common
+        if let Some(val_pair) = histograms[i]
+            .iter()
+            .enumerate()
+            .map(|(chr, count)| (count, chr))
+            .max() {
             let chr = (val_pair.1 as u8) as char;
-            word.push(chr);
-            //println!("{:?} = {}->{}", val_pair, val_pair.1, chr);
+            if chr >= 'a' && chr <= 'z' {
+                most_common.push(chr);
+            }
+        }
+
+        // Least common with non-zero count.
+        if let Some(val_pair) = histograms[i]
+            .iter()
+            .enumerate()
+            .map(|(chr, count)| (count, chr))
+            .filter(|&(count, _)| *count > 0)
+            .min() {
+            let chr = (val_pair.1 as u8) as char;
+            if chr >= 'a' && chr <= 'z' {
+                least_common.push(chr);
+            }
         }
     }
-    println!("Part 1: {}", word);
-    assert!(word == "zcreqgiv")
+    println!("Part 1: '{}'", most_common);
+    assert!(most_common == "zcreqgiv");
 
+    println!("Part 2: '{}'", least_common);
+    assert!(least_common == "pljvorrk");
 }
