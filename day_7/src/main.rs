cbd3f544d7bff3ef727601f60a29c9cf8f31aa7d
Ky Waegel <kwaegel@users.noreply.github.com>
2016-12-08 00:56:13 -0800

    Day 7, part 2

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -2,6 +2,8 @@
 use std::fs::File;
 use std::io::Read;
 
+use std::collections::hash_set::HashSet;
+
 fn main() {
     let mut input_string = String::new();
     let mut file = File::open("input.txt").unwrap();
@@ -13,6 +15,13 @@ fn main() {
 
     println!("Part 1: num valid = {}", num_abba);
     assert!(num_abba == 118);
+
+    let num_ssl = input_string.lines()
+        .filter(|test_ip| valid_ssl(test_ip))
+        .count();
+
+    println!("Part 2: num valid SSL = {}", num_ssl);
+    assert!(num_ssl == 260);
 }
 
 fn valid_abba(input: &str) -> bool {
@@ -55,6 +64,50 @@ fn valid_abba(input: &str) -> bool {
     has_abba
 }
 
+// Valid if aba outside brackets, and bab inside brackets
+fn valid_ssl(input: &str) -> bool {
+    let bytes = input.as_bytes();
+    const OPEN:u8 = '[' as u8;
+    const CLOSE:u8 = ']' as u8;
+
+    // Run a "convolution" kernel abba over the string, checking that
+    // for the strings aba (a==a and a != b) and bab (the inverse)
+    // Keep track of bracket [...] start and stop points.
+    let mut inside_brackets = false;
+
+    let mut aba_set = HashSet::new();
+    let mut bab_set = HashSet::new();
+
+    // We use a 3-wide window, so stop early to avoid running off the end.
+    for i in 0..(bytes.len() - 2) {
+        let has_start = bytes[i] == OPEN || bytes[i + 1] == OPEN || bytes[i + 2] == OPEN;
+        let has_end = bytes[i] == CLOSE || bytes[i + 1] == CLOSE || bytes[i + 2] == CLOSE;
+        inside_brackets = (inside_brackets || has_start) && !(inside_brackets && has_end);
+
+        // Check for the 'xyx' pattern
+        if bytes[i] == bytes[i + 2] && bytes[i] != bytes[i + 1] {
+
+            // Generate the inverse pattern for table query.
+            let xyx = [bytes[i + 1], bytes[i], bytes[i + 1]];
+
+            if inside_brackets {
+                // As 'bab'
+                if aba_set.contains(xyx.as_ref()) {
+                    return true;
+                }
+                bab_set.insert(&bytes[i..i + 3]);
+            } else {
+                // As 'aba'
+                if bab_set.contains(xyx.as_ref()) {
+                    return true;
+                }
+                aba_set.insert(&bytes[i..i + 3]);
+            }
+        }
+    }
+    false
+}
+
 #[test]
 fn test1() {
     assert!(valid_abba("abba[mnop]qrst") == true);
@@ -73,4 +126,24 @@ fn test3() {
 #[test]
 fn test4() {
     assert!(valid_abba("ioxxoj[asdfgh]zxcvbn") == true);
+}
+
+#[test]
+fn ssl_test1() {
+    assert!(valid_ssl("aba[bab]xyz") == true);
+}
+
+#[test]
+fn ssl_test2() {
+    assert!(valid_ssl("xyx[xyx]xyx") == false);
+}
+
+#[test]
+fn ssl_test3() {
+    assert!(valid_ssl("aaa[kek]eke") == true);
+}
+
+#[test]
+fn ssl_test4() {
+    assert!(valid_ssl("zazbz[bzb]cdb") == true);
 }
