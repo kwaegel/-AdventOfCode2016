75ae19246aa2a121e5bddf09931d449175e19061
Ky Waegel <kwaegel@users.noreply.github.com>
2016-12-14 23:51:36 -0800

    Switched to explicit vector of states and iterative deepening search.
    * Still very slow.

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::Read;
 use std::str::FromStr;
 use std::cmp;
+use std::fmt;
 
 #[macro_use]
 extern crate lazy_static;
@@ -38,22 +39,26 @@ impl FromStr for Material {
     }
 }
 
-//-----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-// Materials stored as a bool array, in [chip, generator] pairs.
-// Chip location = material_id*2,
-// Gen  location = material_id*2 + 1
+// Materials stored as a bool array, in [generator, chip] pairs.
+// Gen  location = material_id*2
+// Chip location = material_id*2 + 1,
 const FLOOR_SIZE: usize = 14;
-#[derive(Copy,Clone,Debug)]
+
+#[derive(Debug,PartialEq,Eq,Clone,Copy)]
 struct Floor {
-    items: [bool; FLOOR_SIZE]
+    items: [bool; FLOOR_SIZE],
 }
 impl Floor {
-    // A floor is "safe" if for each material_id*2, the next cell is also set.
+    fn new() -> Floor {
+        Floor { items: [false; FLOOR_SIZE] }
+    }
+    // A floor is "safe" if for each material_id*2, the previous cell is also set.
     fn is_safe(&self) -> bool {
 
         let mut has_gen = false;
-        for gen_id in (1..14).step_by(2) {
+        for gen_id in (0..14).step_by(2) {
             if self.items[gen_id] {
                 has_gen = true;
                 break;
@@ -61,47 +66,51 @@ impl Floor {
         }
         // A floor without generators is always safe.
         if !has_gen {
+            //println!("Safe: no generators");
             return true;
         }
 
-        for chip_id in (0..14).step_by(2) {
-            //println!("Checking [{},{}]", chip_id, chip_id+1);
-            if self.items[chip_id] && !self.items[chip_id+1] {
-//                println!("Not safe! [{}] = {}, [{}] = {}",
-//                         chip_id, self.items[chip_id],
-//                         chip_id+1, self.items[chip_id+1]);
+        for gen_id in (0..14).step_by(2) {
+            let has_gen = self.items[gen_id];
+            let has_chip = self.items[gen_id+1];
+            if has_chip && !has_gen  {
+//                println!("Not safe! gen [{}] = {}, chip [{}] = {}",
+//                         gen_id, self.items[gen_id],
+//                         gen_id+1, self.items[gen_id+1]);
                 return false;
             }
         }
         true
     }
     fn add_chip(&mut self, item: Material) {
-        println!("Adding chip in cell {}", item as usize*2);
-        self.items[item as usize * 2] = true;
+        //println!("Adding chip in cell {}", item as usize * 2+1);
+        self.items[item as usize * 2 + 1] = true;
     }
     fn add_gen(&mut self, item: Material) {
-        println!("Adding gen in cell {}", item as usize*2 + 1);
-        self.items[item as usize * 2 + 1] = true;
+        //println!("Adding gen in cell {}", item as usize * 2);
+        self.items[item as usize * 2] = true;
     }
 }
 
-//-----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-#[derive(Copy,Clone,Debug)]
+#[derive(Debug,PartialEq,Eq,Clone,Copy)]
 struct Building {
-    floors: [Floor; 4]
+    floors: [Floor; 4],
+    elevator_idx: usize,
 }
 impl Building {
     fn new() -> Building {
         Building {
-            floors: [
-            Floor{items: [false; FLOOR_SIZE]},
-            Floor{items: [false; FLOOR_SIZE]},
-            Floor{items: [false; FLOOR_SIZE]},
-            Floor{items: [false; FLOOR_SIZE]},
-            ]
+            floors: [Floor::new(); 4],
+            elevator_idx: 0,
         }
     }
+
+    fn item_exists(&self, item: usize) -> bool {
+        self.floors[self.elevator_idx].items[item]
+    }
+
     fn is_safe(&self) -> bool {
         for floor in self.floors.iter() {
             if !floor.is_safe() {
@@ -112,95 +121,167 @@ impl Building {
     }
     // Check if everything is on the fourth floor (floors 0-2 are empty)
     fn is_final(&self) -> bool {
-        let sum0: usize = self.floors[0].items.iter().map(|&b| if b {1} else {0}).sum();
-        let sum1: usize = self.floors[1].items.iter().map(|&b| if b {1} else {0}).sum();
-        let sum2: usize = self.floors[2].items.iter().map(|&b| if b {1} else {0}).sum();
-        (sum0+sum1+sum2) == 0
+        let sum: usize = self.floors[3].items.iter().map(|&b| if b { 1 } else { 0 }).sum();
+        sum == 4 || sum == 10 // 4 for example data, 10 for input file
     }
-    fn move_up(&mut self, floor: usize, item: usize) {
-        if floor < 3 && self.floors[floor].items[item] {
-            self.floors[floor].items[item] = false;
-            self.floors[floor + 1].items[item] = true;
+
+    // Try to move one or more items up a floor and return the new building state.
+    fn try_move_up(&self, item_1: usize, item_2: usize) -> Option<Building> {
+        if self.elevator_idx < 3 && self.floors[self.elevator_idx].items[item_1] &&
+           self.floors[self.elevator_idx].items[item_2] {
+            let mut next = self.clone();
+
+            next.floors[next.elevator_idx].items[item_1] = false;
+            next.floors[next.elevator_idx + 1].items[item_2] = true;
+
+            next.floors[next.elevator_idx].items[item_2] = false;
+            next.floors[next.elevator_idx + 1].items[item_1] = true;
+
+            next.elevator_idx += 1;
+            if next.is_safe() {
+                Some(next)
+            } else {
+                None
+            }
+        } else {
+            None
         }
     }
-    fn move_down(&mut self, floor: usize, item: usize) {
-        if floor > 0 && self.floors[floor].items[item] {
-            self.floors[floor].items[item] = false;
-            self.floors[floor - 1].items[item] = true;
+
+    // Try to move one or more items up a floor and return the new building state.
+    fn try_move_down(&self, item_1: usize, item_2: usize) -> Option<Building> {
+        if self.elevator_idx > 0 && self.floors[self.elevator_idx].items[item_1] &&
+           self.floors[self.elevator_idx].items[item_2] {
+            let mut next = self.clone();
+
+            next.floors[next.elevator_idx].items[item_1] = false;
+            next.floors[next.elevator_idx - 1].items[item_2] = true;
+
+            next.floors[next.elevator_idx].items[item_2] = false;
+            next.floors[next.elevator_idx - 1].items[item_1] = true;
+
+            next.elevator_idx -= 1;
+            if next.is_safe() {
+                Some(next)
+            } else {
+                None
+            }
+        } else {
+            None
         }
     }
 }
+impl fmt::Display for Building {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for i in 0..4 {
+            let n = 3-i;
+            write!(f, "F{} {} ", n, if n==self.elevator_idx {"E"} else {"."})?;
+            for item in 0..FLOOR_SIZE {
+                write!(f, "{}", if self.floors[n].items[item] {"# "} else {". "})?;
+            }
+            writeln!(f, "")?;
+        }
+        write!(f, "")
+    }
+}
 
-//-----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 const NO_PATH: usize = std::usize::MAX - 1;
-const MAX_DEPTH: usize = 12;
+static mut MAX_DEPTH: usize = 50;
 
 // Returns the number of steps for everything to reach floor 4 (NO_PATH on failure)
-fn process(input_state: &Building, current_floor: usize, depth: usize) -> usize {
+// fn process(input_state: &Building, current_floor: usize, depth: usize) -> usize {
 
-    if depth > MAX_DEPTH {
-        return NO_PATH;
+// Input: stack of building states, the current one at states[states.len()-1];
+// Returns: size of the stack when is_final() on the last state is true.
+fn process(states: &mut Vec<Building>) -> usize {
+
+    let current_idx = states.len() - 1;
+
+    // println!("Checking state {}", current_idx);
+
+    unsafe {
+        if states.len() > MAX_DEPTH {
+            return NO_PATH;
+        }
     }
 
-    if input_state.is_final() {
-        return 0;
+    if states[current_idx].is_final() {
+        // Print the list of moves for debugging
+
+//        println!("Found solution at state vector size {}", states.len());
+//        for state in states {
+//            println!("{}", state);
+//        }
+//        panic!("ended early for debugging");
+
+
+        println!("Found solution at state vector size {}", states.len());
+
+        return states.len();
     }
 
     let mut fewest_steps = NO_PATH;
 
     // Pick pairs of items to move
     for item_1 in 0..FLOOR_SIZE {
-
-        if !input_state.floors[current_floor].items[item_1] {
+        // Need at least one item to move
+        if !states[current_idx].item_exists(item_1) {
             continue;
         }
 
-        // Starting the inner loop from item_1 cover the case of moving just one item.
+        // When item_2 == item_1, we only move one item.
         for item_2 in item_1..FLOOR_SIZE {
-            if !input_state.floors[current_floor].items[item_2] {
+            if !states[current_idx].item_exists(item_2) {
                 continue;
             }
 
+            // println!("Moving items {} and {}", item_1, item_2);
+
             // Try moving both items up or down
-            if current_floor < 3 {
-                let steps = {
-                    let mut next_state = input_state.clone();
-                    next_state.move_up(current_floor, item_1);
-                    next_state.move_up(current_floor, item_2);
-                    if next_state.is_safe() {
-                        process(&next_state, current_floor + 1, depth + 1) + 1
-                    } else {
-                        NO_PATH
-                    }
-                };
-                fewest_steps = cmp::min(fewest_steps, steps);
+            let next_up = states[current_idx].try_move_up(item_1, item_2);
+            if let Some(next_state) = next_up {
+                // Check against prior state back to avoid backtracking.
+                if current_idx <= 1 || next_state != states[current_idx - 1] {
+                    states.push(next_state);
+                    let steps = process(states);
+                    fewest_steps = cmp::min(fewest_steps, steps);
+                    states.pop();
+                }
             }
 
-            if current_floor > 0 {
-                let steps = {
-                    let mut next_state = input_state.clone();
-                    next_state.move_down(current_floor, item_1);
-                    next_state.move_down(current_floor, item_2);
-                    if next_state.is_safe() {
-                        process(&next_state, current_floor - 1, depth + 1) + 1
-                    } else {
-                        NO_PATH
-                    }
-                };
-                fewest_steps = cmp::min(fewest_steps, steps);
+            let next_down = states[current_idx].try_move_down(item_1, item_2);
+            if let Some(next_state) = next_down {
+                // Check against prior state back to avoid backtracking.
+                if current_idx <= 1 || next_state != states[current_idx - 1] {
+                    states.push(next_state);
+                    let steps = process(states);
+                    fewest_steps = cmp::min(fewest_steps, steps);
+                    states.pop();
+                }
             }
-
         }
     }
-//    if fewest_steps < NO_PATH {
-//        println!("Path: {}", fewest_steps);
-//    }
-
     fewest_steps
 }
 
-//-----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+// Iterative deepening
+fn process_id(states: &mut Vec<Building>) -> usize {
+    for i in 3..20 {
+        println!("Trying depth {}...", i);
+        unsafe { MAX_DEPTH = i; }
+        let steps = process(states);
+        if steps < NO_PATH {
+            return steps;
+        }
+    }
+    NO_PATH
+}
+
+// -----------------------------------------------------------------------------
 
 fn read_input(input: &str) -> Building {
     let regex_chip = Regex::new("([:alpha:]+)-compatible").unwrap();
@@ -211,13 +292,13 @@ fn read_input(input: &str) -> Building {
 
         for cap in regex_chip.captures_iter(line) {
             let chip_type = cap.at(1).unwrap().parse::<Material>().unwrap();
-            println!("Floor {}: {:?} type microchip", floor_idx, chip_type);
+            //println!("Floor {}: {:?} type microchip", floor_idx, chip_type);
             building.floors[floor_idx].add_chip(chip_type);
         }
 
         for cap in regex_gen.captures_iter(line) {
             let gen_type = cap.at(1).unwrap().parse::<Material>().unwrap();
-            println!("Floor {}: {:?} type generator", floor_idx, gen_type);
+            //println!("Floor {}: {:?} type generator", floor_idx, gen_type);
             building.floors[floor_idx].add_gen(gen_type);
         }
     }
@@ -225,29 +306,62 @@ fn read_input(input: &str) -> Building {
     building
 }
 
-//-----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 fn main() {
     let mut input_string = String::new();
     let mut file = File::open("input.txt").unwrap();
     let _ = file.read_to_string(&mut input_string);
 
-    let  building = read_input(&input_string);
-    let steps = process(&building, 0, 0);
+    let building = read_input(&input_string);
+
+    let mut states = Vec::new();
+    unsafe { states.reserve(MAX_DEPTH+1); }
+    states.push(building);
+
+    println!("Searching for solution...");
+    let mut steps = process_id(&mut states);
+    steps -= 1; // Subtract 1 for the initial state.
+
     println!("Part 1: steps = {:?}", steps);
     assert!(steps == 11);
 }
 
 #[test]
-fn test() {
-    let test_input =
-    "The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.\n\
-    The second floor contains a hydrogen generator.\n\
-    The third floor contains a lithium generator.\n\
-    The fourth floor contains nothing relevant.\n";
+fn test_example_input() {
+    let test_input = "The first floor contains a hydrogen-compatible microchip and a \
+                      lithium-compatible microchip.\nThe second floor contains a hydrogen \
+                      generator.\nThe third floor contains a lithium generator.\nThe fourth floor \
+                      contains nothing relevant.\n";
 
     let building = read_input(&test_input);
-    let steps = process(&building, 0, 0);
-    println!("Part 1: steps = {:?}", steps);
+    assert!(building.is_safe());
+
+    //println!("Building:\n{}", &building);
+
+    let mut states = Vec::new();
+    unsafe { states.reserve(MAX_DEPTH+1); }
+    states.push(building);
+
+    println!("Searching for solution...");
+    let mut steps = process_id(&mut states);
+    steps -= 1; // Subtract 1 for the initial state.
+
+    println!("Example data: steps = {:?}", steps);
+    assert!(steps < NO_PATH);
     assert!(steps == 11);
+}
+
+#[test]
+fn safety_test() {
+    let mut building = Building::new();
+
+    building.floors[1].add_chip(Material::Hydrogen);
+    building.floors[1].add_gen(Material::Hydrogen);
+    building.floors[1].add_chip(Material::Lithium);
+
+    building.floors[2].add_gen(Material::Lithium);
+
+    println!("Building:\n{}", &building);
+    assert!(!building.is_safe());
 }
