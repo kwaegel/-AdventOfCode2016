976edb035e50fe790790d1b2c163b540721e78bb
Ky Waegel <kwaegel@users.noreply.github.com>
2016-12-21 00:34:16 -0800

    Day 12, part 1

---
+++ day_12/src/main.rs
@@ -0,0 +1,135 @@
+
+use std::fs::File;
+use std::io::Read;
+use std::str::FromStr;
+
+
+#[derive(Debug,PartialEq,Eq,Clone,Copy)]
+enum Register {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+}
+impl FromStr for Register {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Register, &'static str> {
+        match s {
+            "a" => Ok(Register::A),
+            "b" => Ok(Register::B),
+            "c" => Ok(Register::C),
+            "d" => Ok(Register::D),
+            _ => Err("Unknown register string"),
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+#[derive(Debug,PartialEq,Eq,Clone,Copy)]
+enum Source {
+    Reg (Register),
+    Val (i32),
+}
+impl FromStr for Source {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Source, &'static str> {
+
+        //s.parse::<Register>().or_else(Source::Val(s.parse::<i32>())?)
+
+        if let Ok(reg) = s.parse::<Register>() {
+            Ok(Source::Reg(reg))
+        } else {
+            s.parse::<i32>().map(|i| Source::Val(i))
+                .map_err(|_| "Failed to parse int")
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+// cpy x y copies x (either an integer or the value of a register) into register y.
+// inc x increases the value of register x by one.
+// dec x decreases the value of register x by one.
+// jnz x y jumps to an instruction y away (positive means forward; negative means backward),
+//      but only if x is not zero.
+#[derive(Debug,PartialEq,Eq,Clone,Copy)]
+enum Instruction {
+    Cpy {src: Source, dst: Register},
+    Inc {dst: Register},
+    Dec {dst: Register},
+    Jnz {src: Source, offset: i32},
+}
+impl FromStr for Instruction {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Instruction, &'static str> {
+        let tokens: Vec<_> = s.split(' ').collect();
+        match tokens[0] {
+            "cpy" => Ok(Instruction::Cpy{ src: tokens[1].parse()?, dst: tokens[2].parse()? }),
+            "inc" => Ok(Instruction::Inc{dst: tokens[1].parse()?}),
+            "dec" => Ok(Instruction::Dec{dst: tokens[1].parse()?}),
+            "jnz" => Ok(Instruction::Jnz{
+                src: tokens[1].parse()?,
+                offset: tokens[2].parse().expect("Failed to parse int")
+            }),
+            _ => Err("Unable to parse instruction"),
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+fn main() {
+    let mut input_string = String::new();
+    let mut file = File::open("input.txt").unwrap();
+    let _ = file.read_to_string(&mut input_string);
+
+    //input_string = "cpy 41 a\ninc a\ninc a\ndec a\njnz a 2\ndec a".to_owned();
+
+
+    let instructions: Vec<_> = input_string
+        .lines()
+        .map(|line| line.parse::<Instruction>().expect("parse failure"))
+        .collect();
+
+
+//    for ins in instructions {
+//        println!("{:?}", ins);
+//    }
+
+    let mut pc = 0;
+    let mut registers = [0i32; 4];
+    while pc < instructions.len() {
+        //std::thread::sleep_ms(500);
+        let ins = instructions[pc];
+        //println!("{},  {:32?},  {:?}", pc, ins, registers);
+        match ins {
+            Instruction::Cpy{src, dst} => {
+                match src {
+                    Source::Reg(src) => {registers[dst as usize] = registers[src as usize]},
+                    Source::Val(i) => {registers[dst as usize] = i},
+                };
+                pc += 1;
+            },
+            Instruction::Inc{dst} => {registers[dst as usize] += 1; pc += 1;},
+            Instruction::Dec{dst} => {registers[dst as usize] -= 1; pc += 1;},
+            Instruction::Jnz{src, offset} => {
+                let src_val = match src {
+                    Source::Reg(src) => {registers[src as usize]},
+                    Source::Val(i) => i,
+                };
+                if src_val != 0 {
+                    let new_pc = pc as i32 + offset;
+                    pc = new_pc as usize;
+                } else { pc += 1;}
+                //pc += 1;
+            },
+        }
+    }
+
+    println!("Registers: {:?}", registers);
+    assert!(registers[Register::A as usize] == 318007);
+}
