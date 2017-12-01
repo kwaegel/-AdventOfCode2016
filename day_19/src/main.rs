39fe9471dd039c7f03ffbaa4083048d3cfffd0cc
Ky Waegel <kwaegel@users.noreply.github.com>
2017-12-01 01:27:09 -0800

    Day 19, part 2 (horrible solution)
    * I used *absolutely* the wrong data structure here, but the Rust standard library didn't seem to have the correct type and I didn't feel like writing my own.

--- day_19/src/main.rs
+++ day_19/src/main.rs
@@ -1,4 +1,5 @@
 
+use std::collections::VecDeque;
 
 
 fn steal(num_elves: usize) -> usize {
@@ -34,6 +35,40 @@ fn steal(num_elves: usize) -> usize {
     }
 }
 
+fn steal_across(initial_elves: usize) -> usize {
+
+    // Note: A deque is a *terrible* data structure for this problem.
+    // A Linked List or boost-like Deque would be *much* faster.
+    let mut elf_ids: VecDeque<_> = (0..initial_elves).collect();
+
+    let mut thief_idx = 0;
+    while elf_ids.len() > 1 {
+
+        // Identify the victim across the circle
+        let half_size = elf_ids.len()/2;
+        let victim_idx = (thief_idx + half_size) % elf_ids.len();
+
+        // Transfer
+        elf_ids.remove(victim_idx);
+
+        // Relocate the thief, which is now off by one.
+        if thief_idx > victim_idx {
+            thief_idx = thief_idx - 1 % elf_ids.len();
+        } else {
+            thief_idx = thief_idx % elf_ids.len();
+        }
+
+        // Advance the thief index
+        thief_idx = (thief_idx+1) % elf_ids.len();
+
+        if elf_ids.len() % 1000 == 0 {
+            println!("{}", elf_ids.len());
+        }
+    }
+
+    elf_ids[0]
+}
+
 
 fn main() {
 
@@ -42,4 +77,13 @@ fn main() {
 
     let part1_idx = steal(3005290);
     println!("Part 1: ring size 3005290 = winner is elf {}", part1_idx+1);
+    assert_eq!(part1_idx+1, 1816277);
+
+
+    let across_idx = steal_across(5);
+    println!("Part 2: Ring size 5 = winner is elf {}", across_idx+1);
+
+    let part2_idx = steal_across(3005290);
+    println!("Part 2: ring size 3005290 = winner is elf {}", part2_idx+1);
+    assert_eq!(part2_idx+1, 1410967);
 }
